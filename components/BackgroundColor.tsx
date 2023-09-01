2dcde0cb3f4fd60e19f306010d9b15b870fb320f
DollaHane <shanehubsch@gmail.com>
2023-09-01 05:41:58 +0200

    Updated tailwind elements

--- components/BackgroundColor.tsx
+++ components/BackgroundColor.tsx
@@ -11,15 +11,15 @@ export default function BackgroundColor() {
     <div className="fixed">
       {theme === "light" ? (
         <div>
-          <div className="blob absolute right-0 top-0 z-10 h-[400px] w-[400px] rounded-full bg-opacity/60 bg-gradient-to-r from-colorOne via-colorTwo to-colorThr blur-3xl" />
-          <div className="blob rounded-52 absolute left-96 z-10 h-[250px] w-[800px] -rotate-45 bg-opacity/60 bg-gradient-to-r from-colorFou via-colorFiv to-colorSix blur-3xl" />
-          <div className="blob rounded-52 absolute left-96 top-40 z-10 h-[250px] w-[800px] -rotate-45 bg-opacity/60 bg-gradient-to-r from-colorSev via-colorEig to-colorNin blur-3xl" />
+          <div className="blob bg-opacity/60 absolute right-0 top-0 z-10 h-[400px] w-[400px] rounded-full bg-gradient-to-r from-colorOne via-colorTwo to-colorThr blur-3xl" />
+          <div className="blob rounded-52 bg-opacity/60 absolute left-96 z-10 h-[250px] w-[800px] -rotate-45 bg-gradient-to-r from-colorFou via-colorFiv to-colorSix blur-3xl" />
+          <div className="blob rounded-52 bg-opacity/60 absolute left-96 top-40 z-10 h-[250px] w-[800px] -rotate-45 bg-gradient-to-r from-colorSev via-colorEig to-colorNin blur-3xl" />
         </div>
       ) : (
         <div>
-          <div className="blob absolute right-0 top-0 z-10 h-[400px] w-[400px] rounded-full bg-opacity/60 bg-gradient-to-r from-colorOne via-colorTwo to-colorThr blur-3xl" />
-          <div className="blob rounded-52 absolute left-96 z-10 h-[250px] w-[800px] -rotate-45 bg-opacity/60 bg-gradient-to-r from-colorFou via-colorFiv to-colorSix blur-3xl" />
-          <div className="blob rounded-52 absolute left-96 top-40 z-10 h-[250px] w-[800px] -rotate-45 bg-opacity/60 bg-gradient-to-r from-colorSev via-colorEig to-colorNin blur-3xl" />
+          <div className="blob bg-opacity/60 absolute right-0 top-0 z-10 h-[400px] w-[400px] rounded-full bg-gradient-to-r from-colorOne via-colorTwo to-colorThr blur-3xl" />
+          <div className="blob rounded-52 bg-opacity/60 absolute left-96 z-10 h-[250px] w-[800px] -rotate-45 bg-gradient-to-r from-colorFou via-colorFiv to-colorSix blur-3xl" />
+          <div className="blob rounded-52 bg-opacity/60 absolute left-96 top-40 z-10 h-[250px] w-[800px] -rotate-45 bg-gradient-to-r from-colorSev via-colorEig to-colorNin blur-3xl" />
         </div>
       )}
     </div>
--- components/ui/ImageSkeleton.tsx
+++ components/ui/ImageSkeleton.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Image } from 'lucide-react'
+import React from "react"
+import { Image } from "lucide-react"
 
-export default function () {
+export default function ImageSkeleton() {
   return (
-    <div className='flex w-full h-[200px] bg-slate-300 justify-center animate-pulse rounded-lg shadow-lg'>
-      <Image className="w-1/2 h-1/2 text-slate-400 my-auto"/>
+    <div className="flex h-[200px] w-full animate-pulse justify-center rounded-lg bg-slate-300 shadow-lg">
+      <Image className="my-auto h-1/2 w-1/2 text-slate-400" alt="image-skeleton"/>
     </div>
   )
 }
