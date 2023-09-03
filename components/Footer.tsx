bb9e641336fb9473c8ec7b456095cf693fb0a158
DollaHane <shanehubsch@gmail.com>
2023-09-03 11:38:50 +0200

    Updated footer width

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Footer() {
   return (
-    <div className="bottom-0 flex h-12 w-[100vw] grid-cols-1 ">
+    <div className="bottom-0 mx-auto flex h-12 w-[90vw] grid-cols-1">
       <p className="my-auto h-5 w-full justify-center text-center text-xs">
         Created by PepperMint Inc | Copyright © 2023 - Silan Industries Pty
         (Ltd)
