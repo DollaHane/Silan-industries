b7c2de4b8356d8d6658f086cd32915698af8f5e0
DollaHane <shanehubsch@gmail.com>
2024-03-26 12:17:12 +0200

    Updated default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,7 +86,7 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ["Prompt", ...fontFamily.sans],
         opensans: ['Open Sans', ...fontFamily.sans],
         prompt: ['Prompt', ...fontFamily.sans],
         promptbold600: ['Prompt:wght@600', ...fontFamily.sans]
